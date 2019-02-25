08e36af7b41f874e17d3b700f88898fc51ae65c9
Matthieu <matthieunadeau96@gmail.com>
2019-02-25 11:55:13 -0500

    added Picker component

--- App.js
+++ App.js
@@ -3,13 +3,31 @@ import {
   View,
   Text,
   StyleSheet,
+  Picker,
 } from 'react-native';
 
 export default class App extends Component {
+
+  state = {
+    seconds: 5,
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>I'm the App component</Text>
+        <Text style={styles.text}>
+          Choose your notification time in seconds.
+        </Text>
+        <Picker
+          style={styles.picker}
+          selectedValue={this.state.seconds}
+          onValueChange={(seconds) => this.setState({ seconds })}
+          >
+          <Picker.Item label="5" value={5} />
+          <Picker.Item label="10" value={10} />
+          <Picker.Item label="15" value={15} />
+
+        </Picker>
       </View>
     );
   }
@@ -18,5 +36,16 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 10,
   },
+  text: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  picker: {
+    width: 100,
+  }
 });
